86e2d84aa74b9250112a2e501fd050757bed3073
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 12:42:17 +1000

    Completed 4 passing tests of 6

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ class Breakfast {
         this.food = food;
         this.drink = drink;
     }
-}
+};
 
-let bfastOne = new Breakfast (eggs, juice);
+let bfastOne = new Breakfast ("eggs", "juice");
+
+
+class Lunch {
+    constructor(salad,soup,drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+};
+
+
+let lunchOne = new Lunch ("side salad", "broccoli cheddar soup", "iced tea");
+
+
+
 
